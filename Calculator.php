4fbdcf1d4cb1a1842cf83dee4127388b956ffbb7
Stanislav Krasimirov Yordanov <34340580+Stanislav-Yordanov@users.noreply.github.com>
2018-05-10 09:54:34 +0300

    Add files via upload

---
+++ Calculator.php
@@ -0,0 +1,86 @@
+<?php
+namespace CalculatorBundle\Entity;
+
+class Calculator
+{
+    /**
+     * @var float
+     */
+public $leftOperand;
+
+/**
+ * @var float
+ */
+public $rightOperand;
+
+/**
+ * @var string
+ */
+public $operator;
+    /**
+     * Get left operand
+     *
+     * @return float
+     */
+    public function getLeftOperand()
+    {
+        return $this->leftOperand;
+    }
+    /**
+     * Set left operand
+     *
+     * @param float $operand
+     *
+     * @return Calculator
+     */
+    public function setLeftOperand($operand)
+    {
+      $this->leftOperand=$operand;
+      return $this;
+    }
+    /**
+     * Get right operand
+     *
+     * @return float
+     */
+    public function getRightOperand()
+    {
+        return $this->rightOperand;
+    }
+    /**
+     * Set right operand
+     *
+     * @param float $operand
+     *
+     * @return Calculator
+     */
+    public function serRightOperand($operand)
+    {
+        $this->rightOperand=$operand;
+        return $this;
+    }
+
+    /**
+     * Get operator
+     *
+     * @return float
+     */
+    public function getOperator()
+    {
+      return $this->operator;
+    }
+
+    /**
+     * Set operator
+     *
+     * @param string $operator
+     *
+     * @return Calculator
+     */
+    public function setOperator($operator)
+    {
+        $this->operator=$operator;
+
+        return $this;
+    }
+}
